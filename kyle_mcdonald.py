e7fa1de2468ce76c564608e6baf11dbc32324163
Brian Whitman <brian@variogr.am>
2021-04-05 08:30:58 -0400

    kyle's song needed updating

--- kyle_mcdonald.py
+++ kyle_mcdonald.py
@@ -1,9 +1,9 @@
 import alles
 from time import sleep
-clients = alles.sync().keys()
+clients = list(alles.sync().keys())
 try:
     for i in range(400): 
-    	alles.tone(wave=alles.FM,
+    	alles.send(wave=alles.FM,
     		note=[[60,58][(i//32)%2],[48,52][(i//48)%2]][(i//64)%2]+[0,3,5,7,10,11][i%6]*2,
     		vel=0.1*(i%9),
     		patch=14+i%2,
