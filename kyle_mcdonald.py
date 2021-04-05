06985b1b7ff88f8841952d624595e1a0ea69e7fb
Brian Whitman <brian@variogr.am>
2021-04-05 08:28:56 -0400

    kyle's song needed updating

--- kyle_mcdonald.py
+++ kyle_mcdonald.py
@@ -5,11 +5,11 @@ try:
     for i in range(400): 
     	alles.tone(wave=alles.FM,
     		note=[[60,58][(i//32)%2],[48,52][(i//48)%2]][(i//64)%2]+[0,3,5,7,10,11][i%6]*2,
-    		amp=0.1*(i%9),
+    		vel=0.1*(i%9),
     		patch=14+i%2,
     		client=clients[i%len(clients)])
     	sleep([0.08,0.05,0.1][i%3]*(i%3))
 except KeyboardInterrupt:
     pass
-alles.off()
+alles.reset()
 
