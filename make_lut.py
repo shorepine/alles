1c80f225606e38b3e027185043d5e6c9bb3a43e1
Brian Whitman <brian@variogr.am>
2021-04-28 07:41:49 -0700

    make_lut > 4 spaces

--- make_lut.py
+++ make_lut.py
@@ -12,11 +12,11 @@ tab_size = 1024
 
 # 64 equal-amplitude cosines make a band-limited impulse.
 def cosines(num_cosines, args):
-  num_points = len(args)
-  vals = np.zeros(num_points)
-  for i in range(num_cosines):
-    vals += np.cos((i + 1) * args)
-  return vals / float(num_cosines)
+    num_points = len(args)
+    vals = np.zeros(num_points)
+    for i in range(num_cosines):
+        vals += np.cos((i + 1) * args)
+    return vals / float(num_cosines)
 
 #basename = "impulse64"
 #function = partial(cosines, 64)
@@ -37,36 +37,33 @@ cpp_flag = "__" + cpp_base + "_H"
 size_sym = cpp_base + "_SIZE"
 mask_sym = cpp_base + "_MASK"
 
-
 def preamble(f):
-  """Write the stuff at the top of the file."""
-  f.write("// {:s} - lookup table\n".format(filename))
-  f.write("// tab_size = {:d}\n".format(tab_size))
-  f.write("// function = {:s}\n".format(function.__name__))
-  f.write("\n")
-  f.write("#ifndef {:s}\n".format(cpp_flag))
-  f.write("#define {:s}\n".format(cpp_flag))
-  f.write("#define {:s} {:d}\n".format(size_sym, tab_size))
-  f.write("#define {:s} 0x{:x}\n".format(mask_sym, tab_size - 1))
-  f.write("const int16_t {:s}[{:s}] = {{\n".format(basename, size_sym))
-
+    """Write the stuff at the top of the file."""
+    f.write("// {:s} - lookup table\n".format(filename))
+    f.write("// tab_size = {:d}\n".format(tab_size))
+    f.write("// function = {:s}\n".format(function.__name__))
+    f.write("\n")
+    f.write("#ifndef {:s}\n".format(cpp_flag))
+    f.write("#define {:s}\n".format(cpp_flag))
+    f.write("#define {:s} {:d}\n".format(size_sym, tab_size))
+    f.write("#define {:s} 0x{:x}\n".format(mask_sym, tab_size - 1))
+    f.write("const int16_t {:s}[{:s}] = {{\n".format(basename, size_sym))
 
 def postscript(f):
-  """Write stuff at the bottom of the file."""
-  f.write("};\n")
-  f.write("\n")
-  f.write("#endif\n")
-
+    """Write stuff at the bottom of the file."""
+    f.write("};\n")
+    f.write("\n")
+    f.write("#endif\n")
 
 sins = function(np.arange(tab_size) / tab_size * 2 * np.pi)
 row_len = 8
 with open(filename, "w") as f:
-  preamble(f)
-  for base in np.arange(0, tab_size, row_len):
-    for offset in np.arange(row_len):
-      val = int(round(32767 * sins[base + offset]))
-      _ = f.write("{:d},".format(val))
-    _ = f.write("\n")
-  postscript(f)
+    preamble(f)
+    for base in np.arange(0, tab_size, row_len):
+        for offset in np.arange(row_len):
+            val = int(round(32767 * sins[base + offset]))
+            _ = f.write("{:d},".format(val))
+        _ = f.write("\n")
+    postscript(f)
 
 print("wrote", filename)
