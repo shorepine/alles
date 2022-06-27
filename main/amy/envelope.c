9081ec7cfd23564de84a3308f8a9f2fa0b5bb400
Brian Whitman <brian@variogr.am>
2022-06-27 08:21:58 -0400

    fixing #31

--- main/amy/envelope.c
+++ main/amy/envelope.c
@@ -120,6 +120,9 @@ float compute_breakpoint_scale(uint8_t osc, uint8_t bp_set) {
     if(t1 < 0 || v1 < 0) {
         return 0;
     }
+    if(t1 == t0) { 
+        return v1;
+    }
     // OK, we are transition from v0 to v1 , and we're at elapsed time between t0 and t1
     float time_ratio = ((float)(elapsed - t0) / (float)(t1 - t0));
     // Compute scale based on which type we have
@@ -128,10 +131,10 @@ float compute_breakpoint_scale(uint8_t osc, uint8_t bp_set) {
         if(debug_on)printf("%lld [%d,%d] LIN t0 %d v0 %f t1 %d v1 %f elapsed %lld tr %f scale %f\n", total_samples, bp_set, osc, t0, v0, t1, v1, elapsed, time_ratio, scale);
         return scale;
     } else if(synth[osc].breakpoint_target[bp_set] & TARGET_TRUE_EXPONENTIAL) {
+        if(v0 < MINIMUM_SCALE) v0=MINIMUM_SCALE;
         float dx7_exponential_rate = -logf(v1/v0) / (t1 - t0);
         float scale = v0 * expf(-dx7_exponential_rate * (elapsed - t0)); 
         //printf("%lld [%d,%d] DX7 t0 %d v0 %f t1 %d v1 %f elapsed %lld sbi %d exprate %f scale %f \n", total_samples, bp_set, osc, t0, v0, t1, v1, elapsed, segment_block_index, dx7_exponential_rate, scale);
-        if(isnan(scale)) return 0; // TODO: do better 
         return scale;
     } else { // "false exponential?"
         if(debug_on)printf("target is %d\n",synth[osc].breakpoint_target[bp_set] & TARGET_TRUE_EXPONENTIAL );
